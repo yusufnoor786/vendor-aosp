f42df03964a17024637e4488a035c84477e23c54
Zabuka_zuzu <zuzu950444@gmail.com>
2022-07-13 22:28:09 +0200

    version: Bump to version 1.2 Jellyfish

--- config/branding.mk
+++ config/branding.mk
@@ -13,8 +13,8 @@ ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 endif
 
 # Altho Props
-Altho_BASE_VERSION = 1.1
-Altho_CODENAME := Thunder_Cloud
+Altho_BASE_VERSION = V-1.2-Jellyfish
+Altho_CODENAME := Jellyfish
 
 CUSTOM_DATE_YEAR := $(shell date -u +%Y)
 CUSTOM_DATE_MONTH := $(shell date -u +%m)
