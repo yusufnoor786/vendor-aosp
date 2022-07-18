d75d4adec2084320f1effed543063b971bab0198
Zabuka_zuzu <zuzu950444@gmail.com>
2022-07-18 23:54:24 +0200

    vendor: Build ThemePicker

--- config/common.mk
+++ config/common.mk
@@ -229,6 +229,10 @@ $(call inherit-product, vendor/aosp/config/audio.mk)
 # Bootanimation
 $(call inherit-product, vendor/aosp/config/bootanimation.mk)
 
+# ThemePicker
+PRODUCT_PACKAGES += \
+   ThemePicker
+
 # Fonts
 #$(call inherit-product, vendor/aosp/config/fonts.mk)
 
